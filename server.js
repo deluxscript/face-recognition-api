775ab46b7217e18c6687ace2a72f6ec8a2ac0715
deluxscript <delsam94@yahoo.com>
2018-05-12 23:19:42 +0100

    port change

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('/profile/:id', (req, res) => {profile.profileHandler(req, res, db)})
 
 app.put('/image', (req, res) => {image.imageHandler(req, res, db)})
 
-app.listen(3001, () => {
-	console.log('App is running on port 3001');
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`App is running on port ${process.env.PORT}`);
 })
